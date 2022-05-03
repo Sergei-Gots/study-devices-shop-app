d7e485f2feddb9525afba5d55bf505657f545cfa
Sergei Gots <Serge.Gots@gmail.com>
2022-05-03 16:14:08 +0300

    Initial Commit

---
+++ src/main/java/com/sergeigots/study/devicesshop/App.java
@@ -0,0 +1,14 @@
+package com.sergeigots.study.devicesshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!\n" +
+                "Welcome to our new Devices Shop!:)");
+    }
+}
