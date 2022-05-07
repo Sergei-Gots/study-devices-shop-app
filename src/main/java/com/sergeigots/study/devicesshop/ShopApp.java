f575eca40271df1f4a045735218c6a83bf0bbd50
Sergei Gots <Serge.Gots@gmail.com>
2022-05-07 14:16:54 +0300

    Made from the ShopAssistant-class an interface; add the implementation of ShopAssistan-interface to the ShopApp-class

--- src/main/java/com/sergeigots/study/devicesshop/ShopApp.java
+++ src/main/java/com/sergeigots/study/devicesshop/ShopApp.java
@@ -4,11 +4,55 @@ package com.sergeigots.study.devicesshop;
  * Hello world!
  *
  */
-public class ShopApp
-{
+public class ShopApp implements ShopAssistant {
     public static void main( String[] args )
     {
+
         System.out.println("Hello World!\n" +
                 "Welcome to our new Devices Shop!:)");
+    }
+
+    public void createCustomer(){
+
+    }
+
+    public void updateCustomer(){
+
+    }
+
+    public void createProduct(){
+
+    }
+
+    public void updateProduct(){
+
+    }
+
+    public void createPurchase(){
+
+    }
+
+    public void showCustomerInfo(int customerId){
+
+    }
+
+    public void showProductInfo(int productId){
+
+    }
+
+    public void showPurchaseId(int purchaseId){
+
+    }
+
+    public void showCustomersList(){
+
+    }
+
+    public void showProductsList(){
+
+    }
+
+    public void showPurchasesList(){
+
     }
 }
--- src/main/java/com/sergeigots/study/devicesshop/ShopAssistant.java
+++ src/main/java/com/sergeigots/study/devicesshop/ShopAssistant.java
@@ -1,51 +1,17 @@
 package com.sergeigots.study.devicesshop;
 
-public class ShopAssistant {
-    public void createCustomer(){
 
-    }
-
-    public void updateCustomer(){
-
-    }
-
-    public void createProduct(){
-
-    }
-
-    public void updateProduct(){
-
-    }
-
-    public void createPurchase(){
-
-    }
-
-    public void showCustomerInfo(int customerId){
-
-    }
-
-    public void showProductInfo(int productId){
-
-    }
-
-    public void showPurchaseId(int purchaseId){
-
-    }
-
-    public void showCustomersList(){
-
-    }
-
-    public void showProductsList(){
-
-    }
-
-    public void showPurchasesList(){
-
-    }
-
-
-
+public interface ShopAssistant {
+    public void createCustomer();
+    public void updateCustomer();
+    public void createProduct();
+    public void updateProduct();
+    public void createPurchase();
+    public void showCustomerInfo(int id);
+    public void showProductInfo(int id);
+    public void showPurchaseId(int id);
+    public void showCustomersList();
+    public void showProductsList();
+    public void showPurchasesList();
 
 }
