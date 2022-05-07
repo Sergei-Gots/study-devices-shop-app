7d347d3d3f3b5050111169ad2751f2d89b8e32b6
Sergei Gots <Serge.Gots@gmail.com>
2022-05-07 13:49:27 +0300

    Add ShopAssistant class; refactor CashReceipt-Class to Purchase

--- src/main/java/com/sergeigots/study/devicesshop/Purchase.java
+++ src/main/java/com/sergeigots/study/devicesshop/Purchase.java
@@ -3,14 +3,14 @@ package com.sergeigots.study.devicesshop;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
-public class CashReceipt {
+public class Purchase {
 
     protected LocalDateTime dateTime;
     protected int customerId;
     Collection<SoldLot> soldLots;
 
-    public CashReceipt(LocalDateTime dateTime, Customer customer,
-                       Collection<SoldLot> soldLots){
+    public Purchase(LocalDateTime dateTime, Customer customer,
+                    Collection<SoldLot> soldLots){
         this.customerId = customer.getId();
         this.dateTime = dateTime;
         this.soldLots = soldLots;
